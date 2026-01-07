85811655084b3931e372b1ada76229b11193fde5
Hugo Seguin <seguin.hugo06@gmail.com>
2026-01-07 22:34:32 +0100

    Simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',                // Force les bons chemins
-  build: {
-    outDir: 'dist'          // Dossier de sortie standard pour Vite
-  }
+  plugins: [react()]
 })
